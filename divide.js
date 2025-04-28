6c53877fd2037156cc562b77a10a98b51825f967
PratikMane0112 <pratikmane610@gmail.com>
2025-04-28 07:18:29 +0530

    Add File

---
+++ divide.js
@@ -0,0 +1,247 @@
+/*******************************************************************************
+*
+*	@file divide.js
+*	@brief Divide and conquer algorithm
+*
+*******************************************************************************/
+
+let internalHulls = [[], [], []];
+
+let internalPoints = [[], [], []];
+
+let finalHull = [];
+let finalPoints = [];
+
+/**
+*	Enumeration of the possible state the divide and conquer algorithm can be in
+*
+*	@enum {Integer}
+*/
+const divideSteps = Object.freeze({
+	SPLIT: 0,
+	CALCULATE1: 1,
+	CALCULATE2: 2,
+	CALCULATE3: 3,
+	FUSE: 4,
+	DONE: 5
+});
+
+let divideStep = divideSteps.SPLIT;
+
+/**
+*	Enumeration of the different methods for splitting the points
+*
+*	@enum {String}
+*/
+const splitMethods = Object.freeze({
+	VERTICAL: " Vertical Split ",
+	HORIZONTAL: " Horizontal Split ",
+	RADIAL: " Radial Split ",
+	ANGULAR: " Angular Split "
+});
+
+let splitMethod = splitMethods.HORIZONTAL;
+
+/**
+*	Returns an array with arrays of the given size.
+*
+*	@param array {Array} Array to split
+*
+*	@param chunkSize {Integer} Size of every group
+*/
+function chunkArray(inputArray, chunkSize) {
+	// ES6 Clone Array
+    let array = [...inputArray];
+    let results = [];
+
+    while (array.length) {
+        results.push(array.splice(0, chunkSize));
+    }
+
+    let extra = [];
+    if(results[results.length - 1].length != results[results.length - 2].length) {
+    	extra = results.pop();
+    }
+
+    while(extra.length) {
+    	results[results.length - 1].push(extra.pop());
+    }
+
+    return results;
+}
+
+/**
+*	Calculates the internal hull for one of the subsections after division
+*
+*	@param hullIndex {Integer} Select which hull to calculate from 0 to 2
+*/
+function calculateHull(hullIndex) {
+	frameRate(calculateFrameRate);
+
+	const currentPoint = internalPoints[hullIndex][currentIndex];
+	const nextPoint = internalPoints[hullIndex][nextIndex];
+	const checking = internalPoints[hullIndex][index];
+
+	push();
+	stroke(0, 255, 0);
+	line(currentPoint.x, currentPoint.y, checking.x, checking.y);
+	strokeWeight(3);
+	stroke(255, 0, 0);
+	line(currentPoint.x, currentPoint.y, nextPoint.x, nextPoint.y);
+	pop();
+	const a = p5.Vector.sub(nextPoint, currentPoint);
+	const b = p5.Vector.sub(checking, currentPoint);
+	const cross = a.cross(b);
+
+	if (cross.z < 0) {
+		nextIndex = index;
+	}
+	index++;
+
+	if (index == internalPoints[hullIndex].length) {
+	    if (nextIndex == 0) {
+	    	divideStep++;
+	    	currentIndex = 0;
+			nextIndex = 1;
+			index = 2;
+			if(hullIndex + 1 != internalPoints.length) {
+				internalHulls[hullIndex + 1].push(internalPoints[hullIndex + 1][0].copy());
+	    	}
+	    } else {
+	        internalHulls[hullIndex].push(internalPoints[hullIndex][nextIndex]);
+	        currentIndex = nextIndex;
+	        index = 0;
+	        nextIndex = 0;
+	    }
+	}
+}
+
+/**
+*	Combines the three internal hulls into the final convex hull for all points
+*/
+function fuse() {
+	if(finalPoints.length == 0) {
+		finalPoints = [...internalHulls[0]].concat(internalHulls[1]).concat(internalHulls[2]);
+
+		finalPoints.sort((a, b) => a.x - b.x);
+		finalHull.push(finalPoints[0]);
+	} else {
+		frameRate(calculateFrameRate + 10);
+
+		const currentPoint = finalPoints[currentIndex];
+		const nextPoint = finalPoints[nextIndex];
+		const checking = finalPoints[index];
+
+		push();
+		stroke(0, 255, 0);
+		line(currentPoint.x, currentPoint.y, checking.x, checking.y);
+		strokeWeight(3);
+		stroke(255, 0, 0);
+		line(currentPoint.x, currentPoint.y, nextPoint.x, nextPoint.y);
+		pop();
+		const a = p5.Vector.sub(nextPoint, currentPoint);
+		const b = p5.Vector.sub(checking, currentPoint);
+		const cross = a.cross(b);
+
+		if (cross.z < 0) {
+			nextIndex = index;
+		}
+		index++;
+
+		if (index == finalPoints.length) {
+		    if (nextIndex == 0) {
+		    	divideStep++;
+		    	currentIndex = 0;
+				nextIndex = 1;
+				index = 2;
+				time = 0;
+		    } else {
+		        finalHull.push(finalPoints[nextIndex]);
+		        currentIndex = nextIndex;
+		        index = 0;
+		        nextIndex = 0;
+		    }
+		}
+	}
+}
+
+/**
+*	The Divide & Conquer algorithm
+*/
+function divideAndConquer() {
+	switch(divideStep) {
+		case divideSteps.SPLIT:
+			switch(splitMethod) {
+				case splitMethods.VERTICAL: {
+					points.sort((a, b) => a.x - b.x);
+					internalPoints = chunkArray(points, points.length / 3);
+				}
+				break;
+
+				case splitMethods.HORIZONTAL: {
+					points.sort((a, b) => a.y - b.y);
+					internalPoints = chunkArray(points, points.length / 3);
+				}
+				break;
+
+				case splitMethods.RADIAL: {
+					const w2 = width / 2;
+					const h2 = height / 2;
+					points.sort((a, b) => atan2(a.y - h2, a.x - w2) - atan2(b.y - h2, b.x - w2));
+					internalPoints = chunkArray(points, points.length / 3);
+				}
+				break;
+
+				case splitMethods.ANGULAR: {
+					const w2 = width / 2;
+					const h2 = height / 2;
+					points.sort((a, b) => dist(a.x, a.y, w2, h2) - dist(b.x, b.y, w2, h2));
+					internalPoints = chunkArray(points, points.length / 3);
+				}
+				break;
+			}
+
+			currentIndex = 0;
+			nextIndex = 1;
+			index = 2;
+			internalHulls[0].push(internalPoints[0][0].copy());
+			divideStep++;
+		break;
+
+		case divideSteps.CALCULATE1:
+			drawHull(internalHulls[0], 0);
+			calculateHull(0);
+		break;
+
+		case divideSteps.CALCULATE2:
+			drawHull(internalHulls[0], 0);
+			drawHull(internalHulls[1], 30);
+			calculateHull(1);
+		break;
+
+		case divideSteps.CALCULATE3:
+			drawHull(internalHulls[0], 0);
+			drawHull(internalHulls[1], 30);
+			drawHull(internalHulls[2], 60);
+			calculateHull(2);
+		break;
+
+		case divideSteps.FUSE:
+			drawHull(internalHulls[0], 0);
+			drawHull(internalHulls[1], 30);
+			drawHull(internalHulls[2], 60);
+
+			fuse();
+			drawHull(finalHull, 90);
+		break;
+
+		case divideSteps.DONE:
+			drawHull(finalHull, 90);
+			time++;
+
+			if (time > frameRate() * 4) {
+				reset();
+			}
+		break;
+	}
+}
---
+++ sketch.js
@@ -0,0 +1,365 @@
+/*******************************************************************************
+*
+*	@file sketch.js
+*	@brief A quick sketch showing different methods of generating a convexhull
+*
+*******************************************************************************/
+
+let controlPanel;
+
+let resetButton;
+
+/**
+*	Enumeration of the different methods that can be used to split points when
+*	using divide and conquer
+*
+*	@enum {String}
+*/
+const splitMethods = Object.freeze({
+    HORIZONTAL: "horizontal",
+    VERTICAL: "vertical",
+    RADIAL: "radial",
+    ANGULAR: "angular"
+});
+
+let splitMethod = splitMethods.HORIZONTAL;
+
+/**
+*	Variable that store how far away from the side edges new points can spawn
+*
+*	@type {Integer}
+*/
+let xBuffer;
+
+/**
+*	Variable that store how far away from the top and bottom edges new points
+*	can spawn
+*
+*	@type {Integer}
+*/
+let yBuffer;
+
+/**
+*	Enumeration of the different algorithm types
+*
+*	@enum {String}
+*/
+const algorithms = Object.freeze({
+	MONOTONE: "Monotone Chain",
+	JARVIS: "Jarvis March",
+	DIVIDE: "Divide & Conquer",
+	GRAHAM: "Graham Scan"
+});
+
+/**
+*	Variable that stores the current algorithm tha is being executed
+*
+*	@type {Enum<String>}
+*/
+let algorithm = algorithms.JARVIS;
+
+/**
+*	Pointer for the number of points slider in the DOM
+*
+*	@type {p5.Element}
+*/
+let pointsSlider;
+
+/**
+*	Number of points to be bound by the generated convex hull
+*
+*	@type {Integer}
+*/
+let numPoints = 45;
+
+/**
+*	Pointer for the paragraph which shows the number of points in the DOM
+*
+*	@type {p5.Element}
+*/
+let pointsDisplay;
+
+/**
+*	Size of the points drawn on the canvas
+*
+*	@type {Integer}
+*/
+let pointRadius = 10;
+
+/**
+*	Pointer to select field that selects the executing algorithm
+*
+*	@type {p5.Element}
+*/
+let selectAlgorithm;
+
+
+/**
+*	Pointer to select field that selects the method that is used to split the
+*	points when divide and conquer is running
+*
+*	@type {p5.Element}
+*/
+let selectSplitMethod;
+
+/**
+*	Pointer to select field that selects the direction that the Jarvis march
+*	algorithm finds theconvex hull
+*
+*	@type {p5.Element}
+*/
+let selectAngularDirection;
+
+/**
+*	Reset all algorithm variables to initial values
+*/
+function reset() {
+	hull = [];
+	jarvisStep = 0;
+	leftPointIndex = 0;;
+	currentIndex = -1;
+	nextIndex = 1;
+	index = 2;
+	time = 0;
+
+	internalHulls = [[], [], []];
+	finalHull = [];
+	finalPoints = [];
+	lower = [];
+	upper = [];
+
+	divideStep = divideSteps.SPLIT;
+	jarvisStep = jarvisSteps.LEFT;
+	monotoneStep = monotoneSteps.SORT;
+	grahamStep = grahamSteps.SORT;
+
+	points = [];
+	for(let i = 0; i < numPoints; ++i) {
+		const x = random(xBuffer, width - xBuffer);
+		const y = random(yBuffer, height - yBuffer);
+		const newPoint = createVector(x, y);
+		points.push(newPoint);
+	}
+}
+
+/**
+*	Changes the algorithm depending on what the value of the selectAlgorithm
+*	element is
+*/
+function algorithmSelectEvent() {
+	let selectVal = selectAlgorithm.value();
+	if(selectVal != algorithm) {
+		algorithm = selectVal;
+	}
+
+	if(algorithm == algorithms.DIVIDE) {
+		selectSplitMethod.show();
+	} else {
+		selectSplitMethod.hide();
+	}
+
+	if(algorithm == algorithms.JARVIS) {
+		selectAngularDirection.show();
+	} else {
+		selectAngularDirection.hide();
+	}
+
+	if(algorithm == algorithms.MONOTONE) {
+		// show monotone chain options
+	} else {
+		// hide monotone chain options
+	}
+
+	if(algorithm == algorithms.GRAHAM) {
+		// show graham scan options
+	} else {
+		// hide graham scan options
+	}
+
+	reset();
+}
+
+/**
+*	Changes the method for splitting the points in divide and conquer depending
+*	on the value of the selectSplitMethod element
+*/
+function splitSelectEvent() {
+	selectVal = selectSplitMethod.value();
+	if(selectVal != splitMethod) {
+		splitMethod = selectVal;
+		reset();
+	}
+}
+
+/**
+*	Changes the direction the jarvis march algorithm selects new points for the
+*	convex hull depending on the value of the selectAngularDirection element
+*/
+function angularSelectEvent() {
+	selectVal = selectAngularDirection.value();
+	if(selectVal != angularDirection) {
+		angularDirection = selectVal;
+		reset();
+	}
+}
+
+/**
+*	Draws a convex hull with a given colour
+*
+*	@param hullArray {Array<p5.Vector>} Array of points that amke up the
+*		verticies of the hull.
+*
+*	@param colour {Integer} Value of the hue of the given hull from 0 to 100
+*/
+function drawHull(hullArray, colour) {
+	push();
+	colorMode(HSB, 100);
+	fill(colour, 100, 100, 50);
+	beginShape();
+	for (let i = hullArray.length - 1; i >= 0; i--) {
+		push();
+		stroke(colour, 100,100);
+		strokeWeight(0.3 * pointRadius);
+		ellipse(hullArray[i].x, hullArray[i].y, pointRadius, pointRadius);
+		vertex(hullArray[i].x, hullArray[i].y);
+		pop();
+	}
+	endShape(CLOSE);
+	colorMode(RGB);
+	pop();
+}
+
+/**
+*	Draws the edges of a convex hull with a given colour
+*
+*	@param hullArray {Array<p5.Vector>} Array of points that amke up the
+*		verticies of the hull.
+*
+*	@param colour {Integer} Value of the hue of the given hull from 0 to 100
+*/
+function drawEdges(hullArray, colour) {
+	push();
+	colorMode(HSB, 100);
+	stroke(colour, 100, 100, 50);
+	strokeWeight(0.3 * pointRadius);
+	noFill();
+	beginShape();
+	for (let i = hullArray.length - 1; i >= 0; i--) {
+		ellipse(hullArray[i].x, hullArray[i].y, pointRadius, pointRadius);
+		vertex(hullArray[i].x, hullArray[i].y);
+	}
+	endShape();
+	colorMode(RGB);
+	pop();
+}
+
+/**
+*   p5.js setup function, creates canvas.
+*/
+function setup() {
+	let cnvSize;
+	if(windowWidth > windowHeight) {
+		cnvSize = 0.95 * windowHeight;
+	} else {
+		cnvSize = 0.6 * windowWidth;
+	}
+	let cnv = createCanvas(cnvSize, 0.7 * cnvSize);
+	cnv.parent("sketch");
+	
+	// Set base frame rate
+	frameRate(30);
+
+	pointsSlider = createSlider(12, 100, numPoints, 1);
+	pointsSlider.parent("num-points");
+
+	pointsDisplay = createP(numPoints);
+	pointsDisplay.parent("points-val");
+
+	selectAlgorithm = createSelect();
+	selectAlgorithm.parent("algorithm");
+	selectAlgorithm.option(algorithms.JARVIS);
+	selectAlgorithm.option(algorithms.DIVIDE);
+	selectAlgorithm.option(algorithms.MONOTONE);
+	selectAlgorithm.option(algorithms.GRAHAM);
+	selectAlgorithm.changed(algorithmSelectEvent);
+
+	selectAngularDirection = createSelect();
+	selectAngularDirection.parent("algorithm-options");
+	selectAngularDirection.option(direction.CLOCKWISE);
+	selectAngularDirection.option(direction.ANTICLOCKWISE);
+	selectAngularDirection.changed(angularSelectEvent);
+	selectAngularDirection.hide();
+
+	selectSplitMethod = createSelect();
+	selectSplitMethod.parent("algorithm-options");
+	selectSplitMethod.option(splitMethods.HORIZONTAL);
+	selectSplitMethod.option(splitMethods.VERTICAL);
+	selectSplitMethod.option(splitMethods.RADIAL);
+	selectSplitMethod.changed(splitSelectEvent);
+	selectSplitMethod.hide();
+//TODO (omar: omareq08@gmail.com): ANGULAR split method for dnc notworking
+	// selectSplitMethod.option(splitMethods.ANGULAR);
+
+	resetButton = createButton("Reset", "value");
+	resetButton.parent("reset-button");
+	resetButton.mousePressed(reset);
+
+	xBuffer = 0.05 * width;
+	yBuffer = 0.05 * height;
+
+	points = []; // Clear points array before initialization
+	for(let i = 0; i < numPoints; ++i) {
+		const x = random(xBuffer, width - xBuffer);
+		const y = random(yBuffer, height - yBuffer);
+		const newPoint = createVector(x, y);
+		points.push(newPoint);
+	}
+
+	controlPanel = document.getElementById("control-panel");
+	controlPanel.style.visibility = "visible";
+}
+
+/**
+*   p5.js draw function, is run every frame to create the desired animation
+*/
+function draw() {
+	background(0);
+	fill(255);
+	textSize(height * 0.03);
+	textAlign(LEFT, TOP);
+	noStroke();
+	text(algorithm, 0, 0);
+
+	let sliderVal = pointsSlider.value();
+	if(sliderVal != numPoints) {
+		numPoints = sliderVal;
+		pointsDisplay.elt.innerText = "Number of Points: " + str(numPoints);
+		reset();
+	}
+
+	for (var i = points.length - 1; i >= 0; i--) {
+		ellipse(points[i].x, points[i].y, pointRadius, pointRadius);
+	}
+
+	switch(algorithm) {
+		case algorithms.JARVIS:
+		jarvisMarch();
+		break;
+
+		case algorithms.MONOTONE:
+		monotoneChain();
+		break;
+
+		case algorithms.DIVIDE:
+		divideAndConquer();
+		break;
+
+		case algorithms.GRAHAM:
+		grahamScan();
+		break;
+
+		default:
+		background(255);
+		break;
+	}
+}
